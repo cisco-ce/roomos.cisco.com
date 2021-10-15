ecb7a1deb611bc84e96973caa3e809462e0b11a2
Tore Bjolseth <tbjolset@cisco.com>
2021-10-15 15:46:08 +0200

    make script to generate schema list

---
+++ schemas/make-schema-list.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function makeList() {
+  const dir = __dirname;
+  const files = fs.readdirSync(dir);
+
+  const list = [];
+  files.forEach((file) => {
+    if (file.endsWith('.json') && ! file.includes('schemas.json')) {
+      const name = file.replace('ce-', '').replace('.json', '');
+      list.push({ name, url: file });
+    }
+  });
+  console.log(list);
+}
+
+makeList();
+
